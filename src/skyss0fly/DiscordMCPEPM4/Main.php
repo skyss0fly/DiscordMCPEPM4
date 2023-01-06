6b142bd6afed0e61f538d570ceeb2ddd323ef703
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-01-06 13:57:14 +1000

    Add files via upload

---
+++ src/skyss0fly/DiscordMCPEPM4/Main.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace skyss0fly\DiscordMCPEPM4;
+
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use skyss0fly\DiscordMCPEPM4\Events\EventListener;
+
+class Main extends PluginBase implements Listener
+{
+
+    public $webhook, $username, $startupopt, $shutdownopt, $joinopt, $quitopt, $deathopt, $debugopt, $commandopt, $chaturl, $chatformat, $chatprefix, $chatopt, $chatuser, $pp;
+
+    private $configversion = '1.0.2';
+
+    public function onEnable()
+    {
+        $this->setvars();
+        if ($this->isDisabled()) {
+            return;
+        }
+        if ($this->pp == true) {
+            if ($this->getServer()->getPluginManager()->getPlugin('PurePerms') == true) {
+                $this->getServer()->getLogger()->info('[DiscordMCPEPM4] PurePerms Compatibility Enabled!');
+            } else {
+                $this->getServer()->getLogger()->warning('[DiscordMCPEPM4] PurePerms Compatibility Enabled, but PurePerms could not be found!');
+            }
+        }
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        if ($this->getConfig()->get('start_message') !== '0') {
+            $this->sendMessage($this->webhook, $this->startupopt, 'CONSOLE');
+        }
+    }
+
+    public function onDisable()
+    {
+        if ($this->shutdownopt !== '0' AND !$this->isEnabled()) {
+            $this->sendMessage($this->webhook, $this->shutdownopt);
+        }
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param Command $cmd
+     * @param string $label
+     * @param array $args
+     * @return bool
+     */
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
+    {
+        if ($cmd->getName() == 'discord') {
+            if ($this->commandopt) {
+                $ppa = $this->getServer()->getPluginManager()->getPlugin('PurePerms');
+                if (!isset($args[0])) {
+                    $sender->sendMessage(C::RED . 'Please provide an argument! Usage: /discord (message).');
+                } elseif ($this->pp !== null) {
+                    if ($sender instanceof Player) {
+                        $format = str_replace(['{rank}', '{player}', '{message}'], [C::clean($ppa->getUserDataMgr()->getGroup($sender)), $sender->getName(), implode(' ', $args)], $this->chatformat);
+                        $this->sendMessage($this->chaturl, $format, $sender->getName(), $this->chatuser);
+                    } else {
+                        $this->getServer()->getLogger()->notice('[Discord-MCPE] ' . C::RED . 'You cannot execute this command as console!');
+                    }
+                } else {
+                    $format = str_replace(['{player}', '{message}'], [C::clean($sender->getName()), implode(' ', $args)], $this->chatformat);
+                    $this->sendMessage($this->chaturl, $format, $sender->getName(), $this->chatuser);
+                }
+            } else {
+                $sender->sendMessage(C::RED . 'Sorry, but the owner disabled this option.');
+            }
+        }
+        return true;
+    }
+
+    private function setvars()
+    {
+        $this->saveDefaultConfig();
+        if (key_exists('Version', $this->getConfig()->getAll())) {
+            if ($this->configversion !== $this->getConfig()->get('Version')) {
+                $this->getLogger()->critical('Please update your config!');
+                $this->setEnabled(false);
+                return;
+            }
+        } else {
+            $this->getLogger()->critical('Please update your config!');
+            $this->setEnabled(false);
+            return;
+        }
+        foreach ($this->getConfig()->getAll() as $item) {
+            if (!isset($item) OR $item === '') {
+                $this->getLogger()->info('Please edit your config!');
+                $this->setEnabled(false);
+                return;
+            }
+        }
+        $this->reloadConfig();
+        $this->webhook = $this->getConfig()->get('webhook_url');
+        $this->username = $this->getConfig()->get('username');
+        $this->startupopt = $this->getConfig()->get('start_message');
+        $this->shutdownopt = $this->getConfig()->get('shutdown_message');
+        $this->joinopt = $this->getConfig()->get('join_message');
+        $this->quitopt = $this->getConfig()->get('quit_message');
+        $this->deathopt = $this->getConfig()->get('death_message');
+        $this->debugopt = $this->getConfig()->get('debug');
+        $this->commandopt = $this->getConfig()->get('command');
+        $this->chatformat = $this->getConfig()->get('chat_format');
+        $this->chatprefix = $this->getConfig()->get('chat_prefix');
+        $this->chatopt = $this->getConfig()->get('chat');
+        $this->pp = $this->getConfig()->get('pureperms');
+
+        //Some statements
+        if ($this->getConfig()->get('chat_username') === '0') {
+            $this->chatuser = $this->username;
+        } elseif ($this->getConfig()->get('chat_username') !== '0') {
+            $this->chatuser = $this->getConfig()->get('chat_username');
+        }
+        if ($this->getConfig()->get('chat_url') === '0') {
+            $this->chaturl = $this->webhook;
+        } elseif ($this->getConfig()->get('chat_url') !== '0') {
+            $this->chaturl = $this->getConfig()->get('chat_url');
+        }
+    }
+
+    /**
+     * @param $player
+     * @param $result
+     */
+    public function notify($player, $result)
+    {
+        if ($player === 'nolog') {
+            return;
+        } elseif ($player === 'CONSOLE') {
+            $player = new ConsoleCommandSender();
+        } else {
+            $playerinstance = $this->getServer()->getPlayerExact($player);
+            if ($playerinstance === null) {
+                return;
+            } else {
+                $player = $playerinstance;
+            }
+        }
+        if ($result['success']) {
+            $player->sendMessage(C::AQUA . '[Discord-MCPE] ' . C::GREEN . 'Discord message was sent!');
+        } else {
+            if ($this->getConfig()->get('debug')) {
+                $this->getLogger()->error(C::RED . 'Error: ' . $result['Error']);
+            } else {
+                $this->getLogger()->warning(C::RED . 'Something strange happened. Set debug in config to true to get error message');
+            }
+            $player->sendMessage(C::AQUA . '[Discord-MCPE] ' . C::GREEN . 'Discord message wasn\'t sent!');
+        }
+    }
+
+    /**
+     * @param $webhook
+     * @param $message
+     * @param string $player
+     * @param null $username
+     */
+    public function sendMessage($webhook, $message, string $player = 'nolog', $username = null)
+    {
+        if (!isset($username)) {
+            $username = $this->username;
+        }
+        $curlopts = [
+            'content' => $message,
+            'username' => $username
+        ];
+        $this->getServer()->getScheduler()->scheduleAsyncTask(new Tasks\SendTaskAsync($player, $webhook, serialize($curlopts)));
+    }
+}
---
+++ src/skyss0fly/DiscordMCPEPM4/Tasks/SendTaskAsync.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace skyss0fly\DiscordMCPEPM4\Tasks;
+
+use skyss0fly\DiscordMCPEPM4\Main;
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
+
+class SendTaskAsync extends AsyncTask
+{
+    private $player, $webhook, $curlopts;
+
+    /**
+     * SendTaskAsync constructor.
+     * @param $player
+     * @param $webhook
+     * @param $curlopts
+     */
+    public function __construct($player, $webhook, $curlopts)
+    {
+        $this->player = $player;
+        $this->webhook = $webhook;
+        $this->curlopts = $curlopts;
+    }
+
+    public function onRun()
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->webhook);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(unserialize($this->curlopts)));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($curl);
+        $curlerror = curl_error($curl);
+
+        //Messy code incoming
+        $responsejson = json_decode($response, true);
+        $success = false;
+        $error = 'IDK What happened';
+        if ($curlerror != '') {
+            $error = $curlerror;
+        } elseif (curl_getinfo($curl, CURLINFO_HTTP_CODE) != 204) {
+            $error = $responsejson['message'];
+        } elseif (curl_getinfo($curl, CURLINFO_HTTP_CODE) == 204 OR $response === '') {
+            $success = true;
+        }
+        $result = ['Response' => $response, 'Error' => $error, 'success' => $success];
+        $this->setResult($result, true);
+    }
+
+    /**
+     * @param Server $server
+     */
+    public function onCompletion(Server $server)
+    {
+        $plugin = $server->getPluginManager()->getPlugin('Discord-MCPE');
+        if (!$plugin instanceof Main) {
+            return;
+        }
+        if (!$plugin->isEnabled()) {
+            return;
+        }
+        $plugin->notify($this->player, $this->getResult());
+    }
+}
